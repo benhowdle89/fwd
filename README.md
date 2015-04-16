b6c792745c01f6c47f742df78bc9ecfe9c58d1cf
Ben Howdle <benhowdle89@gmail.com>
2015-04-16 09:05:54 +0100

    commenting

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 A dead simple JavaScript plugin to help manage page-specific JavaScript code in the browser.
 
+## Use case
+
+When you want certain JavaScript to only be ran on certain pages (or certain URL patterns), ie. some form validation code to be initialised when the user is on your contact page.
+
+If you need anything more heavy-lifting than this, I'd suggest looking at [Sammyjs](http://sammyjs.org/docs/routes) which has much deeper support for richer applications.
+
 ## Installation
 
 Just include a reference to fwd.js somewhere in your page
@@ -43,7 +49,7 @@ fwd.links({
 
 Here is a sample "router" made with Fwd. We create a "routes" object which maps URL patterns to callback functions.
 
-When the URL matches any of the patterns, then the relevant callback function is invoked. It also supports dynamic URL parameters, as you can see with `:id`, which gets passed to our `showService` method as `id`.
+When the URL matches any of the patterns, the relevant callback function is invoked. It also supports dynamic URL parameters, as you can see with `:id`, which gets passed to our `showService` method as `id`.
 
 Fwd supports multiple URL patterns, from standard page load URLs, ie. `/about`, or dynamic pushState/hash URL changes, ie. `/#about`.
 
--- fwd.js
+++ fwd.js
@@ -1,53 +1,29 @@
-/**
- * Usage
- */
-
-/*
-
-var fwd = new Fwd();
-
-fwd.links({
-
-	routes: {
-		"about": "about",
-		"services/:id": "showService",
-		"services": "services",
-		"contact": "contact"
-	},
-
-	about: function() {
-		console.log('about');
-	},
-
-	services: function() {
-		console.log('services');
-	},
-
-	showService: function(id) {
-		console.log(id);
-	},
-
-	contact: function() {
-		console.log('contact');
-	}
-
-});
-
-*/
-
-
 (function() {
 
+	/**
+	 * Grabs the leading route name from the URL
+	 * @param  {string} route
+	 * @return {string}
+	 */
 	var getRouteLead = function(route) {
 		var parts;
 		return ((parts = route.split('/')) && parts.length && parts[0]);
 	};
 
+	/**
+	 * Grabs everything past the domain name in a URL, either path or hash
+	 * @return {string}
+	 */
 	var getPath = function() {
 		var path = window.location.pathname.slice(1) || window.location.hash.slice(1);
 		return path;
 	};
 
+	/**
+	 * Will return everything past the leading route name, as an array
+	 * @param  {string} route
+	 * @return {array}
+	 */
 	var getParams = function(route) {
 		var parts;
 		return ((parts = route.split('/')) && parts.length && parts.slice(1));
@@ -63,6 +39,9 @@ fwd.links({
 
 	Fwd.prototype = {
 
+		/**
+		 * Hooks onto the hashchange and popstate events triggered on the window
+		 */
 		bind: function() {
 
 			window.addEventListener('hashchange', function(event) {
@@ -70,18 +49,24 @@ fwd.links({
 				this.run(getPath());
 			}.bind(this));
 
-			window.addEventListener('popstate', function(e) {
-				var path = window.location.pathname;
-				this.run(getPath());
-			}.bind(this));
-
+			if (this.hasPushState) {
+				window.addEventListener('popstate', function(e) {
+					var path = window.location.pathname;
+					this.run(getPath());
+				}.bind(this));
+			}
 		},
 
+		/**
+		 * Saves the user's routes & callbacks to the instance
+		 * @param  {object} options
+		 */
 		links: function(options) {
 			options = options || {};
 			this.routes = options.routes;
 			this.callbacks = {};
 
+			// save all keys to `this.callbacks`, except for the "routes" object
 			Object.keys(options).filter(function(key) {
 				return key !== 'routes';
 			}).forEach(function(key) {
@@ -91,6 +76,10 @@ fwd.links({
 			this.run(getPath());
 		},
 
+		/**
+		 * Updates the URL with a new route name, and then triggers the callbacks
+		 * @param  {string} name
+		 */
 		go: function(name) {
 			if (this.hasPushState) {
 				history.pushState(null, '', (name == '') ? '/' : name);
@@ -101,6 +90,10 @@ fwd.links({
 			}
 		},
 
+		/**
+		 * Takes a URL, parses it, and then invokes the relevant callbacks
+		 * @param  {string} url
+		 */
 		run: function(url) {
 			Object.keys(this.routes).forEach(function run(routeName) {
 				if (run.stop) return;
