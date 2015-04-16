698e5f203f1f1140f2a9ed5fe2ad6e69a3fb8912
Ben Howdle <benhowdle89@gmail.com>
2015-04-16 09:09:48 +0100

    formatting

--- fwd.js
+++ fwd.js
@@ -7,7 +7,7 @@
 	 */
 	var getRouteLead = function(route) {
 		var parts;
-		return ((parts = route.split('/')) && parts.length && parts[0]);
+		return (parts = route.split('/')) && parts.length && parts[0];
 	};
 
 	/**
@@ -26,11 +26,11 @@
 	 */
 	var getParams = function(route) {
 		var parts;
-		return ((parts = route.split('/')) && parts.length && parts.slice(1));
+		return (parts = route.split('/')) && parts.length && parts.slice(1);
 	};
 
 	var Fwd = window.Fwd = function() {
-		this.hasPushState = !!(history.pushState);
+		this.hasPushState = !!history.pushState;
 		this.routes = {};
 		this.bind();
 	};
@@ -41,12 +41,10 @@
 		 * Hooks onto the hashchange and popstate events triggered on the window
 		 */
 		bind: function() {
-
 			window.addEventListener('hashchange', function(event) {
 				var path = window.location.hash.slice(1);
 				this.run(getPath());
 			}.bind(this));
-
 			if (this.hasPushState) {
 				window.addEventListener('popstate', function(e) {
 					var path = window.location.pathname;
@@ -63,14 +61,12 @@
 			options = options || {};
 			this.routes = options.routes;
 			this.callbacks = {};
-
 			// save all keys to `this.callbacks`, except for the "routes" object
 			Object.keys(options).filter(function(key) {
 				return key !== 'routes';
 			}).forEach(function(key) {
 				this.callbacks[key] = options[key];
 			}.bind(this));
-
 			this.run(getPath());
 		},
 
@@ -80,22 +76,23 @@
 		 */
 		go: function(name) {
 			if (this.hasPushState) {
-				history.pushState(null, '', (name == '') ? '/' : name);
+				history.pushState(null, '', name == '' ? '/' : name);
 				var path = window.location.pathname;
 				this.run(getPath());
 			} else {
 				window.location.hash = name;
 			}
 		},
-
+		
 		/**
 		 * Takes a URL, parses it, and then invokes the relevant callbacks
 		 * @param  {string} url
 		 */
 		run: function(url) {
 			Object.keys(this.routes).forEach(function run(routeName) {
-				if (run.stop) return;
-				var sameParamCount = (getParams(url).length == getParams(routeName).length);
+				if (run.stop)
+					return;
+				var sameParamCount = getParams(url).length == getParams(routeName).length;
 				if (new RegExp(getRouteLead(routeName)).test(getRouteLead(url)) && sameParamCount) {
 					var replacedParams = getParams(url),
 						callback = this.routes[routeName],
@@ -105,7 +102,5 @@
 				}
 			}.bind(this));
 		}
-
 	};
-
-})();
+}());
